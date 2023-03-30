e1dc6f7c97a880e8e553a1629c1601c9ce66d1bb
charliefaude <124085923+charliefaude@users.noreply.github.com>
2023-03-30 14:56:40 -0500

    Add files via upload

--- lab_7/js/mtn_peaks.js
+++ lab_7/js/mtn_peaks.js
@@ -1,4 +1,4 @@
-var mtn_peaks={
+var mtn_peaks = {
 "type": "FeatureCollection",
 "name": "mtn_peaks_himalayas",
 "coordinates": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
